6da4bca05a4290a739d1b4ac06c283f26e7cd38a
Andrei Stepanenko <ftvkyo@gmail.com>
2024-05-16 11:19:59 +0100

    feat(svg): add identifiers to slots

--- src/keymap.rs
+++ src/keymap.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 use serde::Deserialize;
-use svg::{node::element::{Group, Rectangle}, Document};
+use svg::{node::element::{Group, Rectangle, Text}, Document};
 
 pub type KeySlotId = String;
 
@@ -26,16 +26,22 @@ impl Keymap {
     pub fn svg(&self) -> Document {
         let key_pad = 0.05;
         let key_stroke = 0.05;
+        let key_label_size = 0.15;
 
         let mut min_x: f32 = 0.0;
         let mut max_x: f32 = 0.0;
         let mut min_y: f32 = 0.0;
         let mut max_y: f32 = 0.0;
 
-        let mut g = Group::new()
-            .set("fill", "grey")
+        let mut rects = Group::new()
+            .set("fill", "none")
             .set("stroke", "black")
             .set("stroke-width", key_stroke);
+
+        let mut texts = Group::new()
+            .set("fill", "black")
+            .set("font-family", "JetBrains Mono")
+            .set("font-size", key_label_size);
 
         for (id, slot) in &self.slots {
             let size = slot.size.unwrap_or((1.0, 1.0));
@@ -47,21 +53,26 @@ impl Keymap {
             let y = pos.1 + key_pad;
 
             let mut rect = Rectangle::new()
-                .set("data-id", id.as_str())
                 .set("width", w)
                 .set("height", h)
                 .set("x", x)
                 .set("y", y);
 
+            let mut text = Text::new(id)
+                .set("x", x + key_stroke * 2.0)
+                .set("y", y + key_stroke + key_label_size);
+
             if let Some(angle) = slot.angle {
                 let centre_x = x + w / 2.0;
                 let centre_y = y + h / 2.0;
-
                 let transform = format!("rotate({} {} {})", angle, centre_x, centre_y);
-                rect = rect.set("transform", transform);
+
+                rect = rect.set("transform", transform.clone());
+                text = text.set("transform", transform.clone());
             }
 
-            g = g.add(rect);
+            rects = rects.add(rect);
+            texts = texts.add(text);
 
             // Naively update mins and maxs.
             // Does not account for rotation now.
@@ -74,6 +85,7 @@ impl Keymap {
 
         Document::new()
             .set("viewBox", (min_x, min_y, max_x, max_y))
-            .add(g)
+            .add(rects)
+            .add(texts)
     }
 }
